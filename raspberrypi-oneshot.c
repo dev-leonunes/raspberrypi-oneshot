6a3f376810dba1861b5ba51bc42205aa740c094f
futuroDevLeo <leonunes07@outlook.com>
2025-02-03 20:49:44 -0300

    feat: configure simulation and initial implementations
    - Configure `diagram.json` for board simulation
    - Configure `wokwi.toml` for board simulation
    - Initial implementations in `raspberrypi-oneshot.c`

--- raspberrypi-oneshot.c
+++ raspberrypi-oneshot.c
@@ -1,14 +1,35 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/clocks.h"
+#include "hardware/pll.h"
 
+const uint LED_GREEN = 11, LED_BLUE = 12, LED_RED = 13;
 
-
-int main()
+void setup()
 {
     stdio_init_all();
 
-    while (true) {
+    clocks_init();
+
+    pll_init(pll_sys, 1, 1500 * MHZ, 6 * MHZ, 1);
+
+    gpio_init(LED_GREEN);
+    gpio_init(LED_BLUE);
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+}
+
+int main()
+{
+    setup();
+
+    while (true)
+    {
         printf("Hello, world!\n");
         sleep_ms(1000);
     }
+
+    return 0;
 }
